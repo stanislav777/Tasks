502bcbc01229bf842d57e7dbdf8aa25b208f5722
stanislav777 <solo222@bk.ru>
2021-04-08 22:36:56 +0300

    1.1

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Message from './Message';
+
+type messageData ={
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+const messageData: messageData  = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Some Name Some NameSome Name Some Name Some Name Some Name Some Name Some Name Some Name Some Name Some Name ',
+    message: 'some text',
+    time: '22:00',
+}
+
+
+function HW1() {
+    return (
+        <div>
+            <hr/>
+            homeworks 1
+
+            {/*should work (должно работать)*/}
+
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW1
